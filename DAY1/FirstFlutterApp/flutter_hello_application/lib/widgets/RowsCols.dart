f9b2796db89b8e06f53668969e34f62d4f75b318
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-05-19 12:05:20 +0530

    ✨ using crosssaxis with column and double.infinity

--- DAY1/FirstFlutterApp/flutter_hello_application/lib/widgets/RowsCols.dart
+++ DAY1/FirstFlutterApp/flutter_hello_application/lib/widgets/RowsCols.dart
@@ -4,26 +4,27 @@ class RowsCols extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.end,
       children: <Widget>[
         Container(
           color: Colors.amber,
           height: 100,
-          width: 100,
+          width: double.infinity,
           padding: EdgeInsets.all(10),
           child: const Text("Hello"),
         ),
         Container(
           color: const Color.fromARGB(255, 66, 184, 72),
           height: 100,
-          width: 100,
+          width: double.infinity,
           padding: EdgeInsets.all(10),
           child: const Text("Hola"),
         ),
         Container(
           color: const Color.fromARGB(255, 204, 96, 81),
           height: 100,
-          width: 100,
+          width: double.infinity,
           padding: EdgeInsets.all(10),
           child: const Text("Bye"),
         ),
