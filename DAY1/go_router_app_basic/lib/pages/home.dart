2f072fbce6710eaf06259dc277d51b61018157c8
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-05-25 11:02:10 +0530

    📝 Navigating to About, Profile and Contact

--- DAY1/go_router_app_basic/lib/pages/home.dart
+++ DAY1/go_router_app_basic/lib/pages/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:go_router_app_basic/routes/app_router_config.dart';
+import 'package:go_router_app_basic/routes/app_routes_constant.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -7,7 +10,33 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(children: [Center(child: Text("Home"))]),
+      body: Column(
+        children: [
+          Center(child: Text("Home")),
+          ElevatedButton(
+            onPressed: () {
+              GoRouter.of(context).push(Uri(path: "/about").toString());
+            },
+            child: Text("Go to About"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              GoRouter.of(
+                context,
+              ).pushNamed(MyAppRouterConstants.profileRouteName);
+            },
+            child: Text("Go to Profile"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              GoRouter.of(
+                context,
+              ).pushNamed(MyAppRouterConstants.contactUsRouteName);
+            },
+            child: Text("Go to Contact Us"),
+          ),
+        ],
+      ),
     );
   }
 }
