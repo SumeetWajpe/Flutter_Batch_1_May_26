2b38f69a9cc9de9586de25358528241c69293331
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-06-01 10:34:32 +0530

    ✨ Defining CRUD operations on Task

---
+++ DAY1/todo_tasklist_sqllite/lib/dbhelper.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite/sqlite_api.dart';
+import 'package:todo_tasklist_sqllite/model.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+  static DatabaseHelper get instance => _instance;
+  // Singleton Pattern
+  DatabaseHelper._internal();
+
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    // init db
+    String path = join(await getDatabasesPath(), 'my_app_database.db');
+    return await openDatabase(path, version: 1, onCreate: _createDb);
+  }
+
+  Future<void> _createDb(Database db, int version) async {
+    await db.execute('''
+CREATE TABLE tasks(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        description TEXT,
+        isCompleted INTEGER NOT NULL DEFAULT 0,
+        createdAt TEXT NOT NULL
+      )
+''');
+  }
+
+  Future<int> insertTask(Task task) async {
+    Database db = await instance.database;
+    return await db.insert('tasks', task.toMap());
+  }
+
+  Future<List<Task>> getAllTasks() async {
+    Database db = await instance.database;
+    List<Map<String, dynamic>> maps = await db.query('tasks');
+    return List.generate(maps.length, (i) {
+      return Task.fromMap(maps[i]);
+    });
+  }
+
+  
+}
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 # Github URL/Repo
 https://github.com/SumeetWajpe/Flutter_Batch_1_May_26.git
+
+# Atomic Design Principle
+https://atomicdesign.bradfrost.com/chapter-2/
